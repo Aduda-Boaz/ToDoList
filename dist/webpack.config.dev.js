0f69a99bf530da808cbb1f2080f6a5918084ca35
Aduda-Boaz <adudaboaz@gmail.com>
2022-01-21 19:40:21 +0300

    fix errors

---
+++ dist/webpack.config.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/* eslint-disable no-undef */
+var path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Development',
+    template: './src/index.html'
+  })],
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource'
+    }]
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
+/* eslint-disable no-undef */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
