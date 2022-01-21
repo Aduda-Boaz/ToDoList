b8aef3a87da2202398d0f68a6cd3078ceb1fe0fc
Aduda-Boaz <adudaboaz@gmail.com>
2022-01-21 11:29:49 +0300

    Modify the index js file

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -6,12 +6,25 @@ var _lodash = _interopRequireWildcard(require("lodash"));
 
 require("./style.css");
 
-var _setList = _interopRequireDefault(require("./setList.js"));
-
 var _store = _interopRequireDefault(require("./store.js"));
+
+var _addRemove = require("./add-remove");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+/* eslint no-restricted-globals: "off", curly: "error" */
+var taskList = [];
+
+if (localStorage.getItem('lists')) {
+  taskList = JSON.parse(localStorage.getItem('lists'));
+  (0, _addRemove.displayList)(taskList);
+  (0, _addRemove.setEventListeners)();
+} else {
+  (0, _store["default"])(taskList);
+  (0, _addRemove.displayList)(taskList);
+  (0, _addRemove.setEventListeners)();
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 /* eslint no-restricted-globals: "off", curly: "error" */
 import _, { create } from 'lodash';
 import './style.css';
-import checkCompleted from './setList.js';
 import Storage from './store.js';
+import { displayList, setEventListeners } from './add-remove';
 
+let taskList = [];
+
+if (localStorage.getItem('lists')) {
+  taskList = JSON.parse(localStorage.getItem('lists'));
+  displayList(taskList);
+  setEventListeners();
+} else {
+  Storage(taskList);
+  displayList(taskList);
+  setEventListeners();
+}
